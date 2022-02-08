8c21a46489fb695588b2be02c6e455ea53adc862
Serine93 <81768920+Serine93@users.noreply.github.com>
2022-02-08 11:14:43 +0100

    Update histogramme.java

--- histogramme.java
+++ histogramme.java
@@ -7,22 +7,25 @@ import fr.unistra.pelican.algorithms.io.ImageLoader;
 import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
 
 public class histogramme {
-	//LUI POSER LA QUESTION LE PLUS PRATIQUE A FAIRE
-	public static ArrayList<String> ROUGE = new ArrayList<>();
-	public static ArrayList<String> BLEU = new ArrayList<>();
-	public static ArrayList<String> VERT = new ArrayList<>();
+	
 	public static void main(String[] args) throws IOException{
-		ROUGE.add("C:\\Users\\bettse2008\\Downloads\\motos\\motos\\000.jpg"); //0
-		Image img = ImageLoader.exec("");
+		Image img = ImageLoader.exec("C:\\Users\\bettse2008\\Downloads\\motos\\motos\\000.jpg");
 		Viewer2D.exec(img);
 		Viewer2D.exec(median(img));
+		int seuile = 240;
+		Image msk = buildBackgroundMask(img, seuile);
 		
-		//faire directement dans le main les appels (R,G,B)
-		
-		
+		int R = 0;
+		double[] r= histogramme(img, R, msk);
+		int G = 1;
+		double[] g= histogramme(img, G, msk);
+		int B = 0;
+		double[] b= histogramme(img, B, msk);
+				
 		
 	}
-	public static double[] histogramme(Image img) {
+	//HISTOGRAMME
+	public static double[] histogramme(Image img, int canal, Image mask) {
         int largeur = img.getXDim();
         int hauteur = img.getYDim();
 
@@ -31,11 +34,14 @@ public class histogramme {
         for(int i = 0; i < histo.length; i++) {
             histo[i] = 0;
         }
+        //0 c'est l'objet 
 
         for(int x = 0; x < largeur; x++) {
             for(int y = 0; y < hauteur; y++) {
                 int valeur = img.getPixelXYBByte(x, y, 0);
-                histo[valeur] += 1;
+                int m = img.getPixelXYBByte(x, y, 0);
+                		if(m==255)
+                		histo[valeur] += 1;
             }
         }
         try {
@@ -75,16 +81,48 @@ public class histogramme {
 	
 	//DIVISER PAR DEUX 
 	public void DiviserParDeux(Image img) {
-		int xMax = img.getXDim();
-		int yMax = img.getXDim();
-		int bMax = img.getXDim();
-		
-		
-		for(int x =1; x)
-		
+		int largeur = img.getXDim();
+        int hauteur = img.getYDim();		
+		  for(int x =1; x<largeur; x++) {
+			  for(int y = 1; y<hauteur; y++) {
+				  
+			  }
+		  }
 	}
-	 
+	
+	// NORMALISATION
 	
 	
-	
+	//CALCUL AUTOMATIQUE DU SEUIL 
+
+	//
+	private static Image buildBackgroundMask(Image i, int seuil) {
+        //objet => 255
+        //fond => 0
+        int largeur = i.getXDim();
+        int hauteur = i.getYDim();
+
+        ByteImage mask = new ByteImage(largeur, hauteur, 1, 1, 1);
+
+        for(int x = 0; x < i.getXDim(); x++) {
+            for(int y = 0; y < i.getYDim(); y++ ) {
+
+                int r = i.getPixelXYBByte(x, y, 0);
+                int g = i.getPixelXYBByte(x, y, 1);
+                int b = i.getPixelXYBByte(x, y, 2);
+
+                if (r <= seuil && g <= seuil && b <= seuil) {
+                    mask.setPixelXYTByte(x, y, 0, 255);
+                }else {
+                    mask.setPixelXYTByte(x, y, 0, 0);
+                }
+
+            }
+        }
+
+        return mask;
 }
+}
+	
+	
+
